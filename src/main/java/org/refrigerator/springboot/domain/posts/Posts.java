986ca94319d09454ffc235fd6ae6dd2a2d01544c
Sunmon <blackmilk274@gmail.com>
2020-03-25 13:57:16 +0900

    Add Posts entity

---
+++ src/main/java/org/refrigerator/springboot/domain/posts/Posts.java
@@ -0,0 +1,31 @@
+package org.refrigerator.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter //lombok : Getter 자동 생성
+@NoArgsConstructor  //lombok : public Posts(){} 기본 생성자 자동 생성
+@Entity //JPA
+public class Posts {
+    @Id //PK(Primary Key)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)  //PK auto increment
+    private Long id;
+
+    @Column(length=500, nullable= false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable =false)
+    private String content;
+
+    private String author;
+
+    @Builder // lombok : 빌더 패턴 클래스 생성
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
