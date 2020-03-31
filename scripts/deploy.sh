1fc51f43a905f80ed382f95c0197c66433fda8e0
sunmon <blackmilk274@gmail.com>
2020-03-31 22:33:18 +0900

    오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,14 @@ REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=pray-for-refrigerator
 
 echo "> Build 파일 복사"
+
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-#CURRENT_PID=$(pgrep -f pray-for-refrigerator*.jar)
 CURRENT_PID=$(pgrep -fl pray-for-refrigerator | grep jar | awk '{print $1}')
-echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -20,7 +21,7 @@ else
     sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
@@ -33,6 +34,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-        -Dspring.profiles.active=real \
-        $JAR_NAME > $REPOSITORY/$nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
