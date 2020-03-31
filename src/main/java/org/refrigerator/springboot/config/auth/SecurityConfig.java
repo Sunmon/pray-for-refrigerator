011fdca09e7a83c1b734b1f3877d066862f74ebe
sunmon <blackmilk274@gmail.com>
2020-03-31 21:08:25 +0900

    profile testcase 추가

--- src/main/java/org/refrigerator/springboot/config/auth/SecurityConfig.java
+++ src/main/java/org/refrigerator/springboot/config/auth/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //권한 관리 대상 지정
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     .anyRequest() .authenticated()
                 //로그아웃 설정
