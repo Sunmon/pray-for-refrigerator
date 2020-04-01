54d42ca4a89dbdab2548bcb426a943ae9c469624
sunmon <blackmilk274@gmail.com>
2020-04-01 13:37:30 +0900

    포트 제대로 안 열리던 버그 수정!!!!!!!!!!!!!!!!1

--- src/main/java/org/refrigerator/springboot/Application.java
+++ src/main/java/org/refrigerator/springboot/Application.java
@@ -4,7 +4,6 @@
 package org.refrigerator.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing  //JPA Auditing JpaConfig로 분리
 @SpringBootApplication
