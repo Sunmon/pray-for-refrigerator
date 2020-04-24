7916ddc1c83890d4fc3ad869137ac3f9ef4adfe3
Sunmon <blackmilk274@gmail.com>
2020-04-24 21:36:35 +0900

    검색화면 줄여도 검색버튼 일자로

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,8 +1,42 @@
 //다른 js와 겹치지 않게 따로 index라는 scope선언
-var index={
-    init: function(){
+var index = {
+    init:          function () {
         var _this = this;
+        _this.nav_toggle();
+        // _this.scroll_effect();
+    },
+    nav_toggle:    function () {
+        $(".menu-icon").on("click", function () {
+            $("nav ul").toggleClass("showing");
+        });
+    },
+    scroll_effect: function () {
+        // $(window).on("scroll", function () {
+            // index.scroll_effect();
+            if ($(window).scrollTop()) {
+                $('nav').addClass('black');
+            } else {
+                $('nav').removeClass('black');
+            }
+        // });
     }
 };
+
+// $(document).ready(function () {
+//     $(".menu-icon").on("click", function () {
+//         $("nav ul").toggleClass("showing");
+//     });
+// });
+
+// Scrolling Effect
+
+$(window).on("scroll", function () {
+    index.scroll_effect();
+//     // if ($(window).scrollTop()) {
+//     //     $('nav').addClass('black');
+//     // } else {
+//     //     $('nav').removeClass('black');
+//     // }
+});
 
 index.init();
